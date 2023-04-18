3bcfa494cb63a98b8085aa0f001178d2cb4661e0
LightningFrisk <bmxcaleb@gmail.com>
2023-04-17 19:25:49 -0500

    Progress STudio

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -1,9 +1,18 @@
 package org.launchcode.studio7;
-
+import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args){
-        new CD()
+        public ArrayList<String> contents = new ArrayList<String>();
+        CD cd = new CD("CD example", 64, 32, contents);
+        //DVD dvd = new DVD("DVD example", 4700, "DVD-R", 1450);
+
+        // TODO: Call each CD and DVD method to verify that they work as expected.
+        cd.spinDisc();
+        //dvd.spinDisc();
+
+        cd.readData();
+        //dvd.readData();
     }
 }
 /*Interface should have
