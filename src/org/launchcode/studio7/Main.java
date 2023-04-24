a3c990e95d3f1149e3d11b4b85b774083a633318
LightningFrisk <bmxcaleb@gmail.com>
2023-04-24 18:47:21 -0500

    commit final changes
    woo

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -3,22 +3,15 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args){
-        public ArrayList<String> contents = new ArrayList<String>();
+        ArrayList<String> contents = new ArrayList<String>();
         CD cd = new CD("CD example", 64, 32, contents);
         //DVD dvd = new DVD("DVD example", 4700, "DVD-R", 1450);
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
-        cd.spinDisc();
+        cd.spinDisk();
         //dvd.spinDisc();
 
-        cd.readData();
+        cd.readDisk();
         //dvd.readData();
     }
 }
-/*Interface should have
-* Spin Disk, Read Disk, Write Disk
-* */
-
-/* Abstract
-Name, MaxStorage, RemainingStorage, Contents
-* */
