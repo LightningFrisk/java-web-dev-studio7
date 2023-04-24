3bcfe66c71916d51e4d18f4ffe28fef10f5dba1b
LightningFrisk <bmxcaleb@gmail.com>
2023-04-24 18:57:38 -0500

    Finish studio

--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -6,7 +6,7 @@ public class Main {
         ArrayList<String> cdcontents = new ArrayList<>();
         ArrayList<String> dvdcontents = new ArrayList<>();
         CD cd = new CD("CD example", 64, 32, cdcontents);
-        DVD dvd = new DVD("CD example", 1028, 1000, dvdcontents);
+        DVD dvd = new DVD("DVD example", 1028, 1000, dvdcontents);
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
         cd.spinDisk();
